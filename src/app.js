08812542d7ed0fd5a4c80c5058076b4ab4808357
jumaniyozov <jumaniyozovse@gmail.com>
2021-01-05 02:56:35 +0500

    Home page done

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import * as ROUTES from './constants/routes';
+import {Home, Browse, SignIn, SignUp} from "./pages";
+
+export default function App() {
+    return (
+        <Router>
+            <Route exact path={ROUTES.HOME} >
+                <Home/>
+            </Route>
+            <Route exact path={ROUTES.BROWSE} >
+                <Browse/>
+            </Route>
+            <Route exact path={ROUTES.SIGN_IN} >
+                <SignIn/>
+            </Route>
+            <Route exact path={ROUTES.SIGN_UP} >
+                <SignUp/>
+            </Route>
+        </Router>
+    );
+}
---
+++ src/pages/signup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export default function SignUp() {
+    return (
+        <>
+            <p>Hello from the signup</p>
+        </>
+    );
+}
+
